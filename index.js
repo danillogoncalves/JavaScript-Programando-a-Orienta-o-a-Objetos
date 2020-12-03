e00b1e14120d77ecc3be5efb4b48edb5a92e1941
Danillo <danillo.ooo@hotmail.com>
2020-12-02 21:42:27 -0300

    01. Repetição de código

---
+++ index.js
@@ -0,0 +1,16 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 8882223309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3Nome = "Alice";
+const cliente3CPF = 88822233309;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1Nome);
