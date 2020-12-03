c386c8c545c1bdf0bb1ab82c1638b9665b1fb409
Danillo <danillo.ooo@hotmail.com>
2020-12-02 22:12:40 -0300

    01. Repetição de código, segunda parte

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 8882223309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
 
-console.log(cliente1Nome);
+cliente2.nome = "Alice";
+cliente2.cpf = 8882223309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1, cliente2);
