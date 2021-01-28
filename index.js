b86cf7a5032b22bad441c8176e4085813ae02dfe
Danillo <danillo.ooo@hotmail.com>
2021-01-28 20:58:49 -0300

    Comportamentos de classes

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
 
 cliente2.nome = "Alice";
 cliente2.cpf = 8882223309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 0;
+
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.saldo = 100;
+
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo.saldo);
+
+console.log(cliente1);
+console.log(cliente2);
