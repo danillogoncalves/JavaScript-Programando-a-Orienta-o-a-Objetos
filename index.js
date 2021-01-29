ffc09d6a4f9db9a29742c1679041c6097fa6ba32
Danillo <danillo.ooo@hotmail.com>
2021-01-29 00:08:40 -0300

    Null e undefined

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
+//const cliente2 = new Cliente();
 
-cliente2.nome = "Alice";
-cliente2.cpf = 8882223309;
+//cliente2.nome = "Alice";
+//cliente2.cpf = 8882223309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -17,7 +17,9 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+// conta2.cliente.cpf = 8882223309;
 conta2.agencia = 102;
 
 console.log(conta2);
