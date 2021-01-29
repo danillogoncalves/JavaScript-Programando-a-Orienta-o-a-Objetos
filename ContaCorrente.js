e25bc810f25cdd4cf6f7831ee4aaf1531f0053e8
Danillo <danillo.ooo@hotmail.com>
2021-01-28 23:14:18 -0300

    Composição de classes

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,5 +1,7 @@
 export class ContaCorrente {
     agencia;
+    cliente;
+
     _saldo = 0;
 
     sacar(valor){
@@ -14,5 +16,10 @@ export class ContaCorrente {
             return
         }
         this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- index.js
+++ index.js
@@ -12,14 +12,17 @@ cliente2.nome = "Alice";
 cliente2.cpf = 8882223309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(-150);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+console.log(conta2);
 
+contaCorrenteRicardo.transferir(200, conta2);
+
+console.log(conta2);
 console.log(contaCorrenteRicardo);
