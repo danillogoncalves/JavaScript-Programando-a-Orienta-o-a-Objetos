c0069a4f29bdcf3cfd182de1ed0c58d80b0a8f25
Danillo <danillo.ooo@hotmail.com>
2021-01-28 21:22:20 -0300

    Atributos Privados

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
-    sacar(valor) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor;
         }
     }
 }
@@ -27,17 +33,11 @@ cliente2.cpf = 8882223309;
 const contaCorrenteRicardo = new ContaCorrente();
 
 contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.saldo = 0;
 
-console.log(contaCorrenteRicardo.saldo);
-
-contaCorrenteRicardo.saldo = 100;
-
-console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
 contaCorrenteRicardo.sacar(50);
 
-console.log(contaCorrenteRicardo.saldo);
-
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
