13cced02b2ba8feef8fbf57cbbc32c4ddf2f7f7c
Danillo <danillo.ooo@hotmail.com>
2021-01-28 22:51:42 -0300

    Modulos JavaScript

---
+++ ContaCorrente.js
@@ -0,0 +1,18 @@
+export class ContaCorrente {
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return
+        }
+        this._saldo += valor;
+    }
+}
--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return
-        }
-        this._saldo += valor;
-    }
-}
+import{Cliente} from "./Cliente.js"
+import{ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 
